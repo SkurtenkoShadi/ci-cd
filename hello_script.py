319d488b178f9c618164e815d66f284b3b3e1871
SkurtenkoShadi <86601920+SkurtenkoShadi@users.noreply.github.com>
2025-10-09 16:23:26 +0300

    Update hello_script.py

--- hello_script.py
+++ hello_script.py
@@ -1,10 +1,10 @@
 def greet():
     print("Hello, World!")
 
-def sum_numbers(a, b):
-    return a + b
+def sum_numbers(a, b, c):
+    return a + b + c
 
 if __name__ == "__main__":
     greet()
-    result = sum_numbers(5, 3)
+    result = sum_numbers(5, 3, 7)
     print(f"Sum: {result}")
